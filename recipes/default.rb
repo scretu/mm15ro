59e102b8b81e1ba25796347ccd84c6e8154143ad
Silvian Cretu <silvian.cretu@gmail.com>
2015-10-03 11:11:04 +0300

    mysqldump and execute, etc

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,16 @@
 package 'htop' do
     action :install
 end
+
+execute 'mysqldump' do
+    command 'mysqldump -hmm15ro.cioi47aioaea.eu-west-1.rds.amazonaws.com -uroot -ppasswordXY mm15ro > /home/silviancretu/mm15ro.sql'
+    action :nothing
+    not_if { File.exists?('/home/silviancretu/mm15ro.sql') }
+end
+
+%W(mysql-client-core-5.6 mysql-client-5.6).each do |pack|
+    package pack do
+        action :install
+        action :run, 'execute[mysqldump]', :delayed
+    end
+end
